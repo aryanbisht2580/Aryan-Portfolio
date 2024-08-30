018b77ce711f276771b89893abe7dfe0538a5409
Aryan Bisht <aryanbisht@Aryans-MacBook-Air-3.local>
2024-08-30 10:43:07 +0530

    handled the double line issue for tech components

--- src/components/Projects/ProjectTech.js
+++ src/components/Projects/ProjectTech.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ProjectTech({text}) {
   return (
     <div className='px-2 py-1 mr-2 rounded-md' style={{backgroundColor:"#dfd7f5"}}>
-      <p className='text-purple-600'>{text}</p>
+      <p className='text-purple-600 text-nowrap'>{text}</p>
     </div>
   )
 }
